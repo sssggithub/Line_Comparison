eda8a0ae8db790556f74cec48270ff072201bb0c
sagar <sagarbehera156@gmail.com>
2022-11-03 10:54:18 +0530

    Diplaying Welcome Massage

---
+++ Line_coparison.java
@@ -0,0 +1,10 @@
+package problem;
+
+public class Line_coparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
